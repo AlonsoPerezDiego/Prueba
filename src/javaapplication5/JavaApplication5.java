9125ceeaf6279e4a522d6a307214e049d19e50ea
Diego <Diego@Diego>
2016-10-27 09:26:20 +0200

    Mostrar + cálculo

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -20,6 +20,7 @@ public class JavaApplication5 {
         
         double base = Double.parseDouble(JOptionPane.showInputDialog("Introduce base"));
         double altura= Double.parseDouble(JOptionPane.showInputDialog("Introduce altura"));
+        JOptionPane.showMessageDialog(null, "El área del triángulo es igual a " + base+altura/2 + "cm^2.");
     }
     
 }
