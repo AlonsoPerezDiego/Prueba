c2f4bc9fc87685090705395e761f65eaf75544f5
Diego <Diego@Diego>
2016-10-27 09:22:44 +0200

    Objeto

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -18,6 +18,11 @@ public class JavaApplication5 {
      */
     public static void main(String[] args) {
         
+        Metodos triangulo = new Metodos();
+        
+        triangulo.setUnidades();
+        triangulo.mostrar();
+        
     }
     
 }
